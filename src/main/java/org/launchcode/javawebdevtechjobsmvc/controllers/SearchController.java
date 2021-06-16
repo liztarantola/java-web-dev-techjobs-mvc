48e52176c455c173dc118d4856e75874ba7a6013
elizabethtarantola <liztarantola@gmail.com>
2021-06-16 17:21:09 -0500

    Part 2 complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,8 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @RequestMapping(value = "results")
+    public String displaySearchResults (Model model, @RequestParam String searchType,  @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+    }
 }
