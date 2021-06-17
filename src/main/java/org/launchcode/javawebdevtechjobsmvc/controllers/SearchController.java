33c13c2d2d5177d182cd63654e3b2702c195dbb2
elizabethtarantola <liztarantola@gmail.com>
2021-06-17 18:57:51 -0500

    Assignment Complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -26,7 +26,15 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
     @RequestMapping(value = "results")
-    public String displaySearchResults (Model model, @RequestParam String searchType,  @RequestParam String searchTerm) {
+    public String displaySearchResults (@RequestParam String searchType,  @RequestParam String searchTerm, Model model) {
         ArrayList<Job> jobs;
+        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
     }
 }
